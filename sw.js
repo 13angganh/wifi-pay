7656f88dd867b7cb24a0afe2694353254fd53248
13angganh <13angganh@gmail.com>
2026-03-13 10:56:59 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wifipay-v10";
+const CACHE = "wifipay-v11";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", e => {
